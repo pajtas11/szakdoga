eeea7f7b5a74ae4023fb58f626db71c44d888849
Anett <anettkova@gmail.com>
2026-04-12 17:32:53 +0200

    relativ config

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-PCR_KITS_JSON_PATH = "C:\\Users\\anett\\Repository\\szakdoga\\pcr_kit_test.json"
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+PCR_KITS_JSON_PATH = BASE_DIR / "pcr_kit_test.json"
